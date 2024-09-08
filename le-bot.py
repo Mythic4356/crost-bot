74af6e783f0c5fd194dd0e03ee709cee2f050f1a
Mythic4356 <78289553+Mythic4356@users.noreply.github.com>
2024-09-08 14:53:32 +0400

    token

--- le-bot.py
+++ le-bot.py
@@ -1,9 +1,11 @@
+import os
 import nextcord
 from nextcord.ext import commands
 from nextcord.ui import Button, View
 from nextcord import Interaction, app_commands
 from nextcord.abc import GuildChannel
 
+TOKEN = os.environ["token"]
 
 intents = nextcord.Intents.default()
 intents.message_content = True
@@ -12,4 +14,4 @@ bot = commands.Bot(command_prefix="quaso ", intents=intents)
 bot.remove_command("help")
 client = nextcord.Client(intents=intents)
 
-bot.run("")
+bot.run(TOKEN)
