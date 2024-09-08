39cb36376a6b0bfb28fc3ccb800bc684e3077128
Mythic4356 <78289553+Mythic4356@users.noreply.github.com>
2024-09-08 16:05:07 +0400

    local run capability
    workflow tweakin

--- le-bot.py
+++ le-bot.py
@@ -1,11 +1,22 @@
 import os
+import json
 import nextcord
 from nextcord.ext import commands
 from nextcord.ui import Button, View
 from nextcord import Interaction
 from nextcord.abc import GuildChannel
 
-TOKEN = os.environ["token"]
+
+try:
+    
+    TOKEN = os.environ["token"]
+    print("Token loaded from environment")
+except:
+    try:
+        TOKEN = json.load(open("../crost-secret/token.json"))['Token']
+        print("Token loaded from local config")
+    except:
+        print("No Token found from environment or config")
 
 intents = nextcord.Intents.default()
 intents.message_content = True
