a24a4a76b2bab7039e9fcfb2fb86daf8005ef506
Mythic4356 <78289553+Mythic4356@users.noreply.github.com>
2024-09-08 14:06:48 +0400

    quaso

--- le-bot.py
+++ le-bot.py
@@ -1,2 +1,13 @@
 import nextcord
-print("croissant")
+from nextcord.ext import commands
+from nextcord.ui import Button, View
+from nextcord import Interaction, app_commands
+from nextcord.abc import GuildChannel
+
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+intents.members=True
+bot = commands.Bot(command_prefix="quaso ", intents=intents)
+bot.remove_command("help")
+client = nextcord.Client(intents=intents)
