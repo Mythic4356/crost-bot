4d0a75a00d5dd2ffc181f6d95cdf808da05eef82
Mythic4356 <78289553+Mythic4356@users.noreply.github.com>
2024-09-10 20:34:31 +0400

    Update db.py

--- db.py
+++ db.py
@@ -5,4 +5,10 @@ import os
 
 config = json.load(open("../crost-secret/token.json"))['FirebaseConfig']
 
+def save():
+    print("lbozo i dindt save")
+
+def load():
+    print("lbozo i dindt load")
+
 firebase.Fireb
